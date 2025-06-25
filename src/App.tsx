922395db8e5e556b222b32ab2165bb115fd9f13e
AhmedElsany29 <ahmedelsany12@gmail.com>
2025-06-25 20:51:06 +0300

    feature/signup-footer-darkmode-translte

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import Signup from "./auth/Signup"
 import Footer from "./components/Footer"
+import Reviews from "./components/LearnerReviews"
 import { ThemeProvider } from "./components/theme-provider"
 
 const App = () => {
   return (
     <ThemeProvider  storageKey="vite-ui-theme">
 
-    <div className="">
+    <div>
        <Signup/>
+       <Reviews/>
        <Footer/>
     </div>
     </ThemeProvider>
