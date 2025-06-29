66eeada2d404ec002995832b3c39bfe35f257a67
mariam <miroayman313@gmail.com>
2025-06-29 19:19:42 +0300

    modify

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import InstructorPage from './components/InstructorPage';
 import CourseProgress from './components/ui/CourseProgress';
 function App() {
- return (
+
+return (
     <Router>
       <Routes>
         <Route path="/instructor" element={<InstructorPage />} />
